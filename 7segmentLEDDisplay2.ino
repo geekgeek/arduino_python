62301cd8970bcf32770a9e9e01ee11f420880f7f
geekgeek <solq87@gmail.com>
2016-01-20 05:25:18 +0100

    Create 7segmentLEDDisplay2.ino

---
+++ 7segmentLEDDisplay2.ino
@@ -0,0 +1,199 @@
+// 4 digit 7 segment led display
+//http://www.instructables.com/id/Arduino-4-digit-7-segment-display/
+//http://arduino.fisch.lu/Uploads/arduino/circuit_TYC-365GWB.png
+int aPin = 2;  //                     A
+int bPin = 3;  //             ________
+int cPin = 4;  //           |                   |
+int dPin = 5;  //       F  |                   |  B
+int ePin = 6;  //           |         G       |
+int fPin = 7;  //            |________|
+int gPin = 8;  //           |                   |
+int GND1 = 9;  //        |                   |
+int GND2 = 10; //   E |                   |   C
+int GND3 = 11; //       |________|
+int GND4 = 12; //       
+int num;       //         D
+int dig1 = 0;
+int dig2 = 0;
+int dig3 = 0;
+int dig4 = 0;
+int timeDelay = 1;
+
+
+void setup()
+{
+  pinMode(aPin, OUTPUT);
+  pinMode(bPin, OUTPUT);
+  pinMode(cPin, OUTPUT);
+  pinMode(dPin, OUTPUT);
+  pinMode(ePin, OUTPUT); 
+  pinMode(fPin, OUTPUT);
+  pinMode(gPin, OUTPUT);
+  pinMode(GND1, OUTPUT);
+  pinMode(GND2, OUTPUT);
+  pinMode(GND3, OUTPUT);
+  pinMode(GND4, OUTPUT);
+  Serial.begin(9600);
+
+}
+void loop()
+{
+  
+  
+  digitalWrite( GND1, LOW);
+  six();
+  delay(timeDelay);
+  digitalWrite( GND1, HIGH);
+
+  digitalWrite( GND2, LOW);
+  zero();
+  delay(timeDelay);
+  digitalWrite( GND2, HIGH);
+
+  digitalWrite( GND3, LOW);
+  nine();
+  delay(timeDelay);
+  digitalWrite( GND3, HIGH);
+
+  digitalWrite( GND4, LOW);
+  eight();
+  delay(timeDelay);
+  digitalWrite( GND4, HIGH);
+
+}
+ 
+void pickNumber(int x){
+   switch(x){
+     case 1: one(); break;
+     case 2: two(); break;
+     case 3: three(); break;
+     case 4: four(); break;
+     case 5: five(); break;
+     case 6: six(); break;
+     case 7: seven(); break;
+     case 8: eight(); break;
+     case 9: nine(); break;
+     default: zero(); break;
+   }
+}
+
+void clearLEDs()
+{  
+  digitalWrite(  2, LOW); // A
+  digitalWrite(  3, LOW); // B
+  digitalWrite(  4, LOW); // C
+  digitalWrite(  5, LOW); // D
+  digitalWrite(  6, LOW); // E
+  digitalWrite(  7, LOW); // F
+  digitalWrite(  8, LOW); // G
+}
+
+void one()
+{
+  digitalWrite( aPin, LOW);
+  digitalWrite( bPin, HIGH);
+  digitalWrite( cPin, HIGH);
+  digitalWrite( dPin, LOW);
+  digitalWrite( ePin, LOW);
+  digitalWrite( fPin, LOW);
+  digitalWrite( gPin, LOW);
+}
+
+void two()
+{
+  digitalWrite( aPin, HIGH);
+  digitalWrite( bPin, HIGH);
+  digitalWrite( cPin, LOW);
+  digitalWrite( dPin, HIGH);
+  digitalWrite( ePin, HIGH);
+  digitalWrite( fPin, LOW);
+  digitalWrite( gPin, HIGH);
+}
+
+void three()
+{
+  digitalWrite( aPin, HIGH);
+  digitalWrite( bPin, HIGH);
+  digitalWrite( cPin, HIGH);
+  digitalWrite( dPin, HIGH);
+  digitalWrite( ePin, LOW);
+  digitalWrite( fPin, LOW);
+  digitalWrite( gPin, HIGH);
+}
+
+void four()
+{
+  digitalWrite( aPin, LOW);
+  digitalWrite( bPin, HIGH);
+  digitalWrite( cPin, HIGH);
+  digitalWrite( dPin, LOW);
+  digitalWrite( ePin, LOW);
+  digitalWrite( fPin, HIGH);
+  digitalWrite( gPin, HIGH);
+}
+
+void five()
+{
+  digitalWrite( aPin, HIGH);
+  digitalWrite( bPin, LOW);
+  digitalWrite( cPin, HIGH);
+  digitalWrite( dPin, HIGH);
+  digitalWrite( ePin, LOW);
+  digitalWrite( fPin, HIGH);
+  digitalWrite( gPin, HIGH);
+}
+
+void six()
+{
+  digitalWrite( aPin, HIGH);
+  digitalWrite( bPin, LOW);
+  digitalWrite( cPin, HIGH);
+  digitalWrite( dPin, HIGH);
+  digitalWrite( ePin, HIGH);
+  digitalWrite( fPin, HIGH);
+  digitalWrite( gPin, HIGH);
+}
+
+void seven()
+{
+  digitalWrite( aPin, HIGH);
+  digitalWrite( bPin, HIGH);
+  digitalWrite( cPin, HIGH);
+  digitalWrite( dPin, LOW);
+  digitalWrite( ePin, LOW);
+  digitalWrite( fPin, LOW);
+  digitalWrite( gPin, LOW);
+}
+
+void eight()
+{
+  digitalWrite( aPin, HIGH);
+  digitalWrite( bPin, HIGH);
+  digitalWrite( cPin, HIGH);
+  digitalWrite( dPin, HIGH);
+  digitalWrite( ePin, HIGH);
+  digitalWrite( fPin, HIGH);
+  digitalWrite( gPin, HIGH);
+}
+
+void nine()
+{
+  digitalWrite( aPin, HIGH);
+  digitalWrite( bPin, HIGH);
+  digitalWrite( cPin, HIGH);
+  digitalWrite( dPin, HIGH);
+  digitalWrite( ePin, LOW);
+  digitalWrite( fPin, HIGH);
+  digitalWrite( gPin, HIGH);
+}
+
+void zero()
+{
+  digitalWrite( aPin, HIGH);
+  digitalWrite( bPin, HIGH);
+  digitalWrite( cPin, HIGH);
+  digitalWrite( dPin, HIGH);
+  digitalWrite( ePin, HIGH);
+  digitalWrite( fPin, HIGH);
+  digitalWrite( gPin, LOW);
+} 
