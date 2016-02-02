28ee3b822a4353778744b6ba45bdf60f2b1acbdf
geekgeek <solq87@gmail.com>
2016-02-02 11:43:15 +0100

    Create arrayLoopSwitchCase_5.ino

---
+++ arrayLoopSwitchCase_5.ino
@@ -0,0 +1,100 @@
+
+
+// How to loop through 2 arrays with switch scenario
+ 
+int pos = 0;    // variable to store the servo position 
+int arrValues[] = {45, 90, 180};
+int arrX = 0;
+
+int var1 = 0;
+
+//Array 2
+int startVa[] = {10,120,40};
+int startX = 0;
+//</>
+
+void setup() 
+{ 
+  Serial.begin(9600);
+} 
+ 
+void loop() 
+{
+   switch (arrX) 
+   {
+      case 0:
+      Serial.println("start Array value 0");
+        delay(100);
+        button10();
+        task1();
+        break;
+      case 1:
+        Serial.println("start Array value 1");
+        delay(100);
+        button10();
+        task2();      
+        break;
+      case 2:
+        Serial.println("start Array value 2");
+        delay(100);
+        task3(); 
+        button10();
+        break;
+      default:
+        Serial.println("finished looping");
+        delay(100);
+        arrX = -1;
+        }
+   arrX = arrX +1;
+   startX = startX +1;
+} 
+
+
+void button1()
+{
+      for(pos = 0; pos <= arrValues[arrX]; pos += 1) // goes from 0 degrees to 180 degrees 
+            {                                  // in steps of 1 degree 
+              Serial.println(pos);              // tell servo to go to position in variable 'pos' 
+              delay(50);                       // waits 15ms for the servo to reach the position 
+            } 
+}
+
+void button10()
+{
+  // Goes from one array value to the next with increment of one
+    while(arrValues[arrX] != startVa[startX])
+    {
+      if(arrValues[arrX] < startVa[startX])
+      {
+      arrValues[arrX] = arrValues[arrX] + 1;
+      Serial.println(arrValues[arrX]);
+      delay(100);
+      }
+    
+      else if(arrValues[arrX] > startVa[startX])
+      {
+      arrValues[arrX] = arrValues[arrX] - 1;
+      Serial.println(arrValues[arrX]);
+      delay(100);
+      }
+    }
+}
+
+
+void task1()
+{
+Serial.println("this is task1");
+delay(10);  
+}
+
+void task2()
+{
+Serial.println("this is task2");
+delay(10);  
+}
+
+void task3()
+{
+Serial.println("this is task3");
+delay(10);  
+}
